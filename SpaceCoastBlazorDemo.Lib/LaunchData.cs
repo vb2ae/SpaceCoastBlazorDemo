5a683fb8ddb5fafe5c05546ce6c650354978910d
Ken Tucker <vb2ae@live.com>
2020-02-13 21:15:48 -0500

    Add project files.

---
+++ SpaceCoastBlazorDemo.Lib/LaunchData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceCoastBlazorDemo.Lib
+{
+    public class LaunchData
+    {
+        public string Title { get; set; }
+        public string SmallImage { get; set; }
+        public string MediumImage { get; set; }
+        public string LargeImage { get; set; }
+
+        public string StartOfLaunchWindow { get; set; }
+
+        public string EndOfLaunchWindow { get; set; }
+        public string LaunchLocation { get; set; }
+
+        public string Description { get; set; }
+
+        public List<LinkPair> Links { get; set; }
+
+        public List<string> Missions { get; set; }
+
+    }
+
+    public class LinkPair
+    {
+        public string Text { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ SpaceCoastBlazorDemo.Lib/LaunchDataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceCoastBlazorDemo.Lib
+{
+    public class LaunchDataService
+    {
+
+        public async Task<LaunchData[]> GetLaunchesAsync()
+        {
+            LaunchData[] launches = null;
+            using (HttpClient http = new HttpClient())
+            {
+                try
+                {
+                    string json = await http.GetStringAsync("https://kentuckerapps.azurewebsites.net/api/spacelaunch");
+                    launches = System.Text.Json.JsonSerializer.Deserialize<LaunchData[]>(json);
+                }
+                catch (Exception ex)
+                {
+                    string message = ex.Message;
+                }
+            }
+            return launches;
+        }
+    }
+}
